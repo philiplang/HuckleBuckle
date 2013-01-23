a142bd95b3cb3cf5bb5792cb99ffca27ba5be331
Philip Lang <philipdlang@gmail.com>
2013-01-22 23:17:33 -0500

    add images and js file

---
+++ action.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".submit").click.hide();
+    var answer = Math.floor(Math.random()*100);
+
+    var guess = document.getElementById('guess').value;
+    function hint(guess) {
+      if (guess === answer) {
+        // return ("Congrats!");
+        console.log("Congrats");
+        $('#answer_hint').html('Congrats!');
+      }
+      else if (guess < answer) {
+        // return ("Too low");
+        console.log("Too Low");
+        $('#answer_hint').html('Too low');
+      }
+      else if (guess > answer) {
+        // return ("Too high");
+        console.log("Too high");
+        $('#answer_hint').html('Too high');
+      }
+    };
+    // show(hint(guess));
+  });
+
