c0136511ec5e42037e721dcb74dd6cb64b6fbd6f
Philip Lang <philipdlang@gmail.com>
2013-01-27 19:44:20 -0500

    Major update to code

--- action.js
+++ action.js
@@ -1,25 +1,135 @@
-$(document).ready(function(){
-    $(".submit").click.hide();
-    var answer = Math.floor(Math.random()*100);
 
-    var guess = $('#guess').val();
-    function hint(guess) {
-      if (guess === answer) {
-        // return ("Congrats!");
-        console.log("Congrats");
-        $('#answer_hint').html('Congrats!');
+//$(document).ready(function(){
+
+$(function (){
+  hint();
+})
+    var answer = Math.floor(Math.random()*100);
+    console.log(answer);
+    var t = null;
+    var prev_distance = null;
+
+    function hint() {   
+  
+    $('#submit').click(submit);
+    $('#guess').keydown(function(e){
+      if (e.keyCode == 13) {
+        submit();
       }
-      else if (guess < answer) {
+    });
+      
+      function submit(){
+        var guess = $('#guess').val();
+        console.log(guess);
+        validate(guess); 
+      }
+
+      function validate(guess) {
+        if(guess == '') {
+          $('#answer_hint').html('Please select a value');
+          return;
+        }else if (guess > 100 || guess != Math.floor(guess) || guess < 1) {
+          $('#answer_hint').html("Error: Must be between 1 and 100").css({color:'red'});
+          return;
+        }
+        
+        distance = Math.abs(guess - answer);
+
+        if (guess == answer) {
+          console.log("Congrats");
+          $('#answer_hint').html('Congrats!').css({color:'#000'});
+        }
+        else if (prev_distance == null) {
+          firstGuess(guess, answer);    
+        } else {
+          hotOrCold(prev_distance, distance);
+        }
+        prev_distance = distance;
+      }
+
+      
+
+    function firstGuess (guess, answer){
+      if (guess < answer) {
         // return ("Too low");
         console.log("Too Low");
-        $('#answer_hint').html('Too low');
+        $('#answer_hint').html('Too low').css({color:'#000'});
       }
       else if (guess > answer) {
         // return ("Too high");
         console.log("Too high");
-        $('#answer_hint').html('Too high');
+        $('#answer_hint').html('Too high').css({color:'#000'});
       }
-    };
-    // show(hint(guess));
-  });
+    }
 
+    function hotOrCold (prev_distance, distance){
+      if (prev_distance > distance){
+        $('#answer_hint').html('Getting Warmer').css({color:'#000'});
+      } 
+      else if (prev_distance < distance){
+        $('#answer_hint').html('Brrr... Colder').css({color:'#000'});
+      }
+
+    }
+
+    $('#newgame').click(function(e){
+      e.preventDefault();
+      answer = Math.floor(Math.random()*100);
+      console.log(answer);
+      $('#answer_hint').html('');
+    });
+
+    $('#cheat').click(function(e){
+      e.preventDefault();
+      alert(answer);
+    });
+
+    };
+
+// });
+
+  /*    $('#submit').click(function(e){
+        e.preventDefault(); 
+        var guess = $('#guess').val();
+        console.log(guess);
+        if (guess > 100 || guess != Math.floor(guess) || guess < 1) {
+          $('#answer_hint').html("Error: Must be between 1 and 100").css({color:'red'});
+          return false;
+        }
+        
+  $('#guess').click(function(event){
+        //console.log(String.fromCharCode(event.keyCode));
+        if (t != null) {
+          clearTimeout(t);
+        }
+        t = setTimeout(timeout, 500);
+      });
+
+      function timeout() {
+        var guess = $('#guess').val();
+        console.log(guess);
+        validate(guess);       
+      }
+
+
+
+        if (guess == answer) {
+          // return ("Congrats!");
+          console.log("Congrats");
+          $('#answer_hint').html('Congrats!');
+        }
+        else if (guess < answer) {
+          // return ("Too low");
+          console.log("Too Low");
+          $('#answer_hint').html('Too low');
+        }
+        else if (guess > answer) {
+          // return ("Too high");
+          console.log("Too high");
+          $('#answer_hint').html('Too high');
+        }
+        return false;
+      });
+
+    };
+*/
